78cc8a30728ac32bea81fb2cd9b56273c83434f4
oosouzaa <130710621+oosouzaa@users.noreply.github.com>
2023-09-28 21:46:07 -0300

    Add files via upload

---
+++ HelloWord.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.helloword;
+
+/**
+ * @athor HG
+ */
+
+import java.util.Scanner;
+
+public class HelloWord {
+
+    public static void main(String[] args) {
+//       Scanner sc = new Scanner(System.in);
+//       String name = sc.nextLine();
+//       String value = "O nome digitado foi: ";
+//       String conc = value.concat(name);
+//       System.out.println(conc);
+
+//        System.out.println("Digite seu ano de nascimento, que vamos dizer sua idade/n");
+//        Scanner sc = new Scanner(System.in);
+//        int year = sc.nextInt();
+//        int yearsOld = 2023-year;
+//        System.out.print("sua idade é: ");
+//        System.out.print(yearsOld);
+//        System.out.print(" anos");
+
+
+//          int controller = 0;
+//          while (controller != 10) {
+//              System.out.println("Digite o valor 10 para sair do loop.");
+//              Scanner sc = new Scanner(System.in);
+//              controller = sc. nextInt();
+//              if(controller == 10){
+//                 System.out.println("saimos do loop.");
+//              } else {
+//                 System.out.println("Vamos rodar novamente.");
+//              }
+          }
+    }
+} 
